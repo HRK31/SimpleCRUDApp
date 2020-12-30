7860458ac9d022f0ad431c94ccb6935c27e83f38
Hritik Kumar <hrk10321@gmail.com>
2020-12-30 16:51:42 +0530

    Added monogdb atlas database to the app

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const express=require('express')
-const app=express()
-const port=3000
+const express = require('express')
+const app = express();
+const mongodb = 'mongodb+srv://root_simplecrud:SimpleCRUDApp@cluster0.ttby9.mongodb.net/item-database?retryWrites=true&w=majority';
+const port = 3000;
 
 app.listen(3000);
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     //res.sendFile('./views/index.html',{root:__dirname});
     const items = [
-        { name: 'Book', price: 1000},
-        { name: 'Notebook', price: 100},
-        { name: 'Pen', price: 20},
-        { name: 'Pencil', price: 10}
+        { name: 'Book', price: 1000 },
+        { name: 'Notebook', price: 100 },
+        { name: 'Pen', price: 20 },
+        { name: 'Pencil', price: 10 }
     ]
-    res.render('index', {items});
+    res.render('index', { items });
 })
 
-app.get('/add-items',(req,res)=>{
+app.get('/add-items', (req, res) => {
     //res.sendFile('./views/add-items.html',{root:__dirname});
     res.render('add-items');
 })
 
-app.use((req,res)=>{
+app.use((req, res) => {
     res.render('error');
 })
