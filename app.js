b423decccd449c175b0ca16c2e1d3af0a1e1b828
Hritik Kumar <hrk10321@gmail.com>
2020-12-30 12:25:10 +0530

    Added ejs template engine

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const app=express()
 const port=3000
 
 app.listen(3000);
+app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-    res.sendFile('./views/index.html',{root:__dirname});
+    //res.sendFile('./views/index.html',{root:__dirname});
+    res.render('index');
 })
 
 app.get('/add-items',(req,res)=>{
-    res.sendFile('./views/add-items.html',{root:__dirname});
+    //res.sendFile('./views/add-items.html',{root:__dirname});
+    res.render('add-items');
 })
